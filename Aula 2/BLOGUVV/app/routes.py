68c7c70ed4d64c1501ff38fe967680a4cc0dbbaf
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2025-02-26 10:52:16 -0300

    Add files via upload

---
+++ Aula 2/BLOGUVV/app/routes.py
@@ -0,0 +1,29 @@
+# app/routes.py
+from flask import render_template
+from app import app
+from random import randint
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'username': 'Matheus'}
+    posts = [
+        {
+        'author': {'username': 'Joao'},
+        'body': 'Belo dia em Vila Velha!'
+        },
+        {
+        'author': {'username': 'Maria'},
+        'body': 'Bora para o cinema hoje?'
+        }
+    ]
+    numero = randint(1, 2)
+    if(numero == 1):
+        img = 'https://preview.redd.it/tdebr2qaf7ee1.jpeg?width=640&crop=smart&auto=webp&s=1ed7f2e0c74463dd8270f5523e42d7736cf8b627'
+    else:
+        img = 'https://preview.redd.it/sexaebx7c9ee1.jpeg?width=640&crop=smart&auto=webp&s=2721b0801f7b37bcf1eafd9ca8c92085da27507c'
+    
+    imgs = [{'a': 'https://s2-ge.glbimg.com/BsRH4rij3O7pZ-gj3JCCLNFcFw0=/0x0:1100x618/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/Y/i/dULbPDQDC1fIKuUQQRug/csgo-image.jpg'},
+            {'a': 'https://www.baixarcs16agora.com/wp-content/uploads/2020/04/counter-strike-cheats-002.jpg'}]
+    return render_template('index.html', title='home', user=user, img=img, posts=posts, imgs = imgs)
