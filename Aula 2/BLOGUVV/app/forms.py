48cc99309b9d3a6549d0b63b506ce73b96d733d4
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2025-03-12 10:44:06 -0300

    Add files via upload

---
+++ Aula 2/BLOGUVV/app/forms.py
@@ -0,0 +1,13 @@
+# forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('remenber_me')
+    submit = SubmitField('submit')
+
+
--- Aula 2/BLOGUVV/app/routes.py
+++ Aula 2/BLOGUVV/app/routes.py
@@ -1,6 +1,7 @@
 # app/routes.py
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 from random import randint
 
 @app.route('/')
@@ -27,3 +28,12 @@ def index():
     imgs = [{'a': 'https://s2-ge.glbimg.com/BsRH4rij3O7pZ-gj3JCCLNFcFw0=/0x0:1100x618/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/Y/i/dULbPDQDC1fIKuUQQRug/csgo-image.jpg'},
             {'a': 'https://www.baixarcs16agora.com/wp-content/uploads/2020/04/counter-strike-cheats-002.jpg'}]
     return render_template('index.html', title='home', user=user, img=img, posts=posts, imgs = imgs)
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login solicitado pelo usuario {}, Remeber ME = {}".format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
